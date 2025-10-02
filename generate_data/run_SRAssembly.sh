e8b6d3b50290b39530b70171b6409e516f71a95b
dwells-eit <dwells@eit.org>
2025-10-02 14:08:11 +0100

    feat: generate test data for Haemophilus influenzae

---
+++ generate_data/run_SRAssembly.sh
@@ -0,0 +1,18 @@
+
+# https://github.com/EIT-Pathogena-Bioinformatics/SRAssembly
+nextflow run ~/pathogena/technology_applications/SRAssembly/EIT-Pathogena-Bioinformatics-getsrassembly/ \
+   -profile docker \
+   -c generate_data/SRAssembly.configs \
+   -with-tower \
+   -resume \
+   --input generate_data/SRAssembly_sample_sheet.csv \
+   --outdir generate_data/SRAssembly_results
+
+
+seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117372_1.fastq.gz 10000 > data/SRR17117372_1_subset.fastq.gz
+seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117372_2.fastq.gz 10000 > data/SRR17117372_2_subset.fastq.gz
+
+seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117371/SRR17117371.fastq 1000 > data/SRR17117371_subsample.fastq
+gzip data/SRR17117371_subsample.fastq
+
+cp generate_data/SRAssembly_results/wget/GCF_020736045.1.genomic.fna.gz data
