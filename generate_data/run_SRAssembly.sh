1adf378011d435dba5dbdd84107f9affafc447ab
dwells-eit <dwells@eit.org>
2025-10-29 15:04:51 +0000

    Fix data generation script

--- generate_data/run_SRAssembly.sh
+++ generate_data/run_SRAssembly.sh
@@ -12,7 +12,6 @@ nextflow run ~/pathogena/technology_applications/SRAssembly/EIT-Pathogena-Bioinf
 seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117372_1.fastq.gz 10000 | gzip > data/SRR17117372_1_subset.fastq.gz
 seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117372_2.fastq.gz 10000 | gzip > data/SRR17117372_2_subset.fastq.gz
 
-seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117371/SRR17117371.fastq 1000 gzip > data/SRR17117371_subsample.fastq
-gzip data/SRR17117371_subsample.fastq
+seqtk sample -s100 generate_data/SRAssembly_results/sratools/SRR17117371/SRR17117371.fastq 1000 | gzip > data/SRR17117371_subsample.fastq.gz
 
 cp generate_data/SRAssembly_results/wget/GCF_020736045.1.genomic.fna.gz data
